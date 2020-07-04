c5d289d65060772a7b23bc46f41283a2f1ae72e0
SergwayKhodyachikh <sergway.khodyachikh@gmail.com>
2020-07-04 12:47:33 +0300

    procced layout

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,21 +1,37 @@
 import React from 'react';
-import { Button, Avatar } from '@material-ui/core';
+import { Button, Avatar, Divider, IconButton } from '@material-ui/core';
 import Cookies from 'js-cookie';
 import Logo from 'components/shared/Logo/Logo';
 import { NavLink } from 'react-router-dom';
 import { FiLogOut } from 'react-icons/fi';
 import { AiOutlineFolder } from 'react-icons/ai';
 import './Dashboard.scss';
+import AppBar from '@material-ui/core/AppBar';
+import { MdAccountCircle } from 'react-icons/md';
+import Menu from '@material-ui/core/Menu';
+
 
 const Dashboard = () => {
   return (
     <div className="dashboard">
       <header className="dashboard__header">
-        <Logo />
-        <div className="user-menu">
-          <Avatar className="user-menu__avatar">SK</Avatar>
-          <span>Sergway</span>
-        </div>
+        <AppBar position="relative" className="dashboard__header__app-bar">
+          <Logo />
+          <div className="user-menu">
+            <Avatar className="user-menu__avatar">SK</Avatar>
+            <span>Sergway</span>
+          </div>
+
+          <IconButton
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={() => alert('test')}
+            color="inherit"
+          >
+            <MdAccountCircle />
+          </IconButton>
+        </AppBar>
       </header>
       <div className="container">
         <nav className="dashboard__navbar">
@@ -46,6 +62,7 @@ const Dashboard = () => {
               </a>
             </li>
           </ul>
+          <Divider orientation="vertical" />
         </nav>
         <div className="dashboard__content">project page</div>
       </div>
